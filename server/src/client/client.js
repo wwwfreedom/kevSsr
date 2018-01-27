976ebdb21dcaabf0e1a914b3cfc575e16b7f92c0
Kevin <tig3r86@gmail.com>
2018-01-28 09:40:37 +1100

    add react router to the ssr project

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,6 +1,12 @@
 // start up point for the client side application
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './components/Home.js'
+import { BrowserRouter } from 'react-router-dom'
+import Routes from './Routes.js'
 
-ReactDOM.hydrate(<Home />, document.querySelector('#root'))
+ReactDOM.hydrate(
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import Home from '../client/components/Home.js'
+import { StaticRouter } from 'react-router-dom'
+import Routes from '../client/Routes.js'
 
-export default () => {
-  const content = renderToString(<Home />)
+export default req => {
+  const content = renderToString(
+    // context is require for static router and is use handle redirect and error handling
+    // Static router need to know the current path this will be past in by the express
+    <StaticRouter location={req.path} context={{}}>
+      <Routes />
+    </StaticRouter>
+  )
 
   return `
     <html>
